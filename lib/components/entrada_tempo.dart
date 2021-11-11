76ff4016d263baeab5d3b4600e6ae0cfe58acf6a
Leandro Rangel <rangel94@outlook.com>
2021-11-11 17:26:59 -0300

    modulo 19(mobx) finalizado!

---
+++ lib/components/entrada_tempo.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
+
+class EntradaTempo extends StatelessWidget {
+  final String titulo;
+  final int valor;
+  final void Function()? inc;
+  final void Function()? dec;
+
+  const EntradaTempo({
+    Key? key,
+    required this.titulo,
+    required this.valor,
+    this.inc,
+    this.dec,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          titulo,
+          style: const TextStyle(
+            fontSize: 18,
+          ),
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: dec,
+              child: const Icon(
+                Icons.arrow_downward,
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(10),
+                primary: store.estaTrabalhando() ? Colors.red : Colors.green,
+              ),
+            ),
+            Text(
+              '$valor min',
+              style: const TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: inc,
+              child: const Icon(
+                Icons.arrow_upward,
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(10),
+                primary: store.estaTrabalhando() ? Colors.red : Colors.green,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
